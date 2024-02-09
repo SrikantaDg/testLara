cd78ae7d6e09b25bb0b04c641d22aad743ce0ce6
SrikantaDg <durga.garai@gmail.com>
2024-02-10 00:00:12 +0530

    welcomecontroller added

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
+use App\Http\Controllers\WelcomeController;
+//Route::get('/',[WelcomeController::class,'index']);
+Route::get('/', 'WelcomeController@index');
